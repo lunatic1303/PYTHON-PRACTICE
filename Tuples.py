3882b26ae23f4e98885010059eb2732a23edf476
Varshaghadge13 <varshaghadge13032008@gmail.com>
2026-06-20 15:02:54 +0530

    Tuple problems

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+"""Problem-Count occurences of an element in tuple"""
+v = (1,2,3,4,5,1,2,1)
+m = int(input("Enter element to count:"))
+print("The element",m,"occurs",v.count(m), "times")
+
+"""Problem-Find maximum and minimum element in tuple"""
+n = (1,2,3,4,5,1,2,1)
+print("Maximum element is:",max(n),"and Minimum element is:",min(n))
+
+"""Problem-Convert list to tuple"""
+n  = (1,2,3,4,5,1,2,1)
+print(list(n))
